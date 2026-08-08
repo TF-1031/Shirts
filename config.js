fda6199e5469c2c901f5b9568551892a8a1b3a36
TF-1031 <toddefarrell@gmail.com>
2026-08-08 17:21:46 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Columbia Prints site settings
+// Edit only this file when you want to change the order deadline or banner message.
+
+window.COLUMBIA_PRINTS_CONFIG = {
+  deadlineISO: "2026-08-10T08:00:00-05:00",
+  deadlineMessage: "Must be submitted by 8:00 AM Central on August 10, 2026."
+};
